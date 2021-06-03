c1e534cc4f7a96a9bd15161218794bb20df98836
Zachary Chiodini <zacharybchiodini@gmail.com>
2021-06-02 23:23:49 -0400

    Updated main and utils

--- flask-api/main.py
+++ flask-api/main.py
@@ -1,74 +1,112 @@
-import json
 import model
-from flask import abort, jsonify, request
+from flask import Response
 from config import app, connect_db, db
+from utils import entity_get_or_post_response, record_id_response
 
 HOST = '127.0.0.1'
 PORT = '5000'
 
 
-@app.route('/author/', methods=['GET', 'POST'])
-def author():
-    if request.method == 'GET':
-        all_authors = model.Author.query\
-            .order_by(model.Author.last_name).all()
-        author_schema = model.AuthorSchema(many=True)
-        response = jsonify(author_schema.dump(all_authors))
-        return response
-    if request.method == 'POST':
-        new_author = json.loads(request.get_json())
-        first_name = new_author.get('first_name')
-        middle_name = new_author.get('middle_name')
-        last_name = new_author.get('last_name')
-        author_already_exists = model.Author.query\
-            .filter(model.Author.first_name == first_name)\
-            .filter(model.Author.middle_name == middle_name)\
-            .filter(model.Author.last_name == last_name)\
-            .one_or_none()
-        if author_already_exists:
-            abort(409, 'Author {} {} {} already exists.'
-                  .format(first_name, middle_name, last_name))
-        author_schema = model.AuthorSchema()
-        new_author = author_schema.load(
-            new_author, session=db.session
-            )
-        db.session.add(new_author)
-        db.session.commit()
-        response = jsonify(author_schema.dump(new_author))
-        return response
-    abort(405, 'Method not allowed.')
+@app.kinetics('/kinetics/')
+def kinetics() -> Response:
+    return entity_get_or_post_response(
+        model.Kinetics, model.KineticsSchema,
+        fk_substance_relationships_id=model.Kinetics.fk_substance_relationships_id,
+        pH=model.Kinetics.pH,
+        pH_min=model.Kinetics.pH_min,
+        pH_max=model.Kinetics.pH_max,
+        half_life=model.Kinetics.half_life,
+        half_life_min=model.Kinetics.half_life_min,
+        half_life_max=model.Kinetics.half_life_max,
+        half_life_units=model.Kinetics.half_life_units,
+        rate=model.Kinetics.rate,
+        rate_min=model.Kinetics.rate_min,
+        rate_max=model.Kinetics.rate_max,
+        rate_units=model.Kinetics.rate_units,
+        reaction=model.Kinetics.reaction,
+        temp_C=model.Kinetics.temp_C,
+        activation_kcal_per_mol=model.Kinetics.activation_kcal_per_mol,
+        comments=model.Kinetics.comments
+        )
 
 
-@app.route('/author_record/<author_id>/',
-           methods={'GET', 'PATCH', 'PUT', 'DELETE'})
-def author_record(author_id):
-    if request.method == 'GET':
-        record = model.Author.query\
-            .filter(model.Author.id == author_id)\
-            .one_or_none()
-        if record is None:
-            abort(404, 'Author with id {} not found.'
-                  .format(author_id))
-        author_schema = model.AuthorSchema()
-        response = jsonify(author_schema.dump(record))
-        return response
-    if request.method == 'PUT':
-        pass
-    abort(405, 'Method not allowed.')
+@app.route('/kinetics_record/<kinetics_id>/')
+def kinetics_record(kinetics_id) -> Response:
+    return record_id_response(
+        model.Kinetics, model.KineticsSchema,
+        fk_substance_relationships_id=model.Kinetics.fk_substance_relationships_id,
+        pH=model.Kinetics.pH,
+        pH_min=model.Kinetics.pH_min,
+        pH_max=model.Kinetics.pH_max,
+        half_life=model.Kinetics.half_life,
+        half_life_min=model.Kinetics.half_life_min,
+        half_life_max=model.Kinetics.half_life_max,
+        half_life_units=model.Kinetics.half_life_units,
+        rate=model.Kinetics.rate,
+        rate_min=model.Kinetics.rate_min,
+        rate_max=model.Kinetics.rate_max,
+        rate_units=model.Kinetics.rate_units,
+        reaction=model.Kinetics.reaction,
+        temp_C=model.Kinetics.temp_C,
+        activation_kcal_per_mol=model.Kinetics.activation_kcal_per_mol,
+        comments=model.Kinetics.comments
+        )
+
+@app.route('/substance_relationships/')
+def substance_relationships() -> Response:
+    return entity_get_or_post_response(
+        model.SubstanceRelationships, model.SubstanceRelationships,
+        fk_generic_substance_id_predecessor=model.SubstanceRelationships
+            .fk_generic_substance_id_predecessor,
+        fk_generic_substance_id_successor=model.SubstanceRelationships
+            .fk_generic_substance_id_successor,
+        relationship=model.SubstanceRelationships.relationship,
+        fk_substance_relationship_type_id=model.SubstanceRelationships
+            .fk_substance_relationship_type_id,
+        source=model.SubstanceRelationships.source,
+        qc_notes=model.SubstanceRelationships.qc_notes,
+        mixture_percentage=model.SubstanceRelationships.mixture_percentage,
+        percentage_type=model.SubstanceRelationships.percentage_type,
+        is_nearest_structure=model.SubstanceRelationships.is_nearest_structure,
+
+        )
+
+
+@app.route('/author/')
+def author() -> Response:
+    return entity_get_or_post_response(
+        model.Author, model.AuthorSchema,
+        first_name=model.Author.first_name,
+        middle_name=model.Author.middle_name,
+        last_name=model.Author.last_name
+        )
+
+
+@app.route('/author_record/<author_id>/')
+def author_record(author_id) -> Response:
+    return record_id_response(
+        model.Author, model.AuthorSchema,
+        first_name=model.Author.first_name,
+        middle_name=model.Author.middle_name,
+        last_name=model.Author.last_name
+        )
 
 
 @app.route('/connect/', methods=['GET'])
-def connect():
+def connect() -> Response:
     try:
-        with open('config.json') as file:
+        with open('flask-api//config.json') as file:
             login_info = json.load(file)
         connect_db(app, db, login_info)
     except Exception as e:
-        return str(e)
-    return 'Connected to {}'.format(db.engine)
+        response = Response(str(e), status=404)
+        return response
+    response = Response('Connected to {}'.format(db.engine),
+                        status=200)
+    return response
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return ''
 
---
+++ flask-api/utils.py
@@ -0,0 +1,142 @@
+import json
+from config import db, ma
+from flask import jsonify, Response, request
+from typing import Dict, List, Union
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+
+def get_features(entity: db.Model) -> Dict:
+    features = {}
+    instantiated = entity()
+    for attr, value in entity.__dict__.items():
+        if (isinstance(value, InstrumentedAttribute)
+                and getattr(instantiated, attr) is None):
+            features[attr] = value
+    return features
+
+
+def query_payload(
+        entity: db.Model,
+        payload: Dict,
+        **features: db.Column
+        ) -> Union[List, None]:
+    filters = []
+    for label, column in features.items():
+        value = payload.get(label)
+        filters.append(column == value)
+    return entity.query.filter(*filters).one_or_none()
+
+
+def entity_get_response(
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema
+        ) -> Response:
+    select_all = entity.query.all()
+    entity_schema = schema(many=True)
+    response = jsonify(entity_schema.dump(select_all))
+    return response
+
+
+def entity_post_response(
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema,
+        **features: db.Column
+        ) -> Response:
+    payload = json.loads(request.get_json())
+    record_already_exists = query_payload(entity,payload,**features)
+    if record_already_exists:
+        response = Response('Record already exists.', status=409)
+        return response
+    entity_schema = schema(many=True)
+    new_record = entity_schema.load(payload, session=db.session)
+    db.session.add(new_record)
+    db.session.commit()
+    response = jsonify(entity_schema.dump(new_record))
+    return response
+
+
+def entity_get_or_post_response(
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema,
+        **features: db.Column
+        ) -> Response:
+    if request.method == 'GET':
+        return entity_get_response(entity, schema)
+    if request.method == 'POST':
+        return entity_post_response(entity, schema, **features)
+    response = Response('Method not allowed.', status=405)
+    return response
+
+
+def record_id_get_response(
+        primary_key: int,
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema,
+        ) -> Response:
+    record = entity.query\
+        .filter(entity.id == primary_key) \
+        .one_or_none()
+    if not record:
+        response = Response('Record id not found.', status=404)
+        return response
+    entity_schema = schema(many=True)
+    response = jsonify(entity_schema.dump(record))
+    return response
+
+
+def record_id_put_response(
+        primary_key: int,
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema,
+        **features: db.Column
+        ) -> Response:
+    record_to_update = entity.query\
+        .filter(entity.id == primary_key)\
+        .one_or_none()
+    if not record_to_update:
+        return entity_post_response(entity, schema, **features)
+    payload = json.loads(request.get_json())
+    existing_record = query_payload(entity, payload, **features)
+    if existing_record and existing_record.id != primary_key:
+        response = Response('Record already exists.', status=409)
+        return response
+    entity_schema = schema()
+    updated_record = entity_schema.load(payload, session=db.session)
+    updated_record.id = record_to_update.id
+    db.session.merge(updated_record)
+    db.session.commit()
+    response = jsonify(entity_schema.dump(updated_record))
+    return response
+
+
+def record_id_delete_response(
+        primary_key: int,
+        entity: db.Model,
+        ) -> Response:
+    record_to_delete = entity.query\
+        .filter(entity.id == primary_key)\
+        .one_or_none()
+    if not record_to_delete:
+        response = Response('Record id not found.', status=404)
+        return response
+    db.session.delete(record_to_delete)
+    db.session.commit()
+    response = Response('Record deleted.', status=200)
+    return response
+
+
+def record_id_response(
+        primary_key: int,
+        entity: db.Model,
+        schema: ma.SQLAlchemyAutoSchema,
+        **features: db.Column
+        ) -> Response:
+    if request.method == 'GET':
+        return record_id_get_response(primary_key, entity, schema)
+    if request.method == 'PUT':
+        return record_id_put_response(
+            primary_key, entity, schema, **features)
+    if request.method == 'DELETE':
+        return record_id_delete_response(primary_key, entity)
+    response = Response('Method not allowed.', status=405)
+    return response
